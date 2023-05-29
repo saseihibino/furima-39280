c818a6866aac8816fa82f3ec62332426130b5bd3
saseihibino <132185275+saseihibino@users.noreply.github.com>
2023-05-29 17:12:36 +0900

    タスク7　商品詳細表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :Prefecture
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :daytosend
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :day_to_send
   belongs_to_active_hash :delivery_charge
   belongs_to :user
   has_one_attached :image
