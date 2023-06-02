b53d0b8838bb861b2a1808db8deaef9224427ce5
saseihibino <132185275+saseihibino@users.noreply.github.com>
2023-06-02 18:59:26 +0900

    タスク10　商品購入機能修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   before do 
     user = FactoryBot.create(:user)
-    item = FactoryBot.create(:item, user_id: user.id)
+    item = FactoryBot.create(:item)
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id) 
   end
 
